1d4d7ba3f422f3efc0ae38e953d6f31f96784a52
larissa <vashappenin0111@gmail.com>
2023-01-11 02:33:53 +0800

    created new page

--- src/router.jsx
+++ src/router.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Home from './home';
 import App from './App';
-
+import LarissaPage from './larissa_page'
 export default function Router() {
     return (
         <Routes>
             <Route index element={<App />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/larissa_page" element={<LarissaPage />} />
         </Routes>
     )
 }
